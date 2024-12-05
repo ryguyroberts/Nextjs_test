988270b256db4cf4ac18bab3cb75bcb7659eb67a
Ryan Robertson <ryguyrobertson@gmail.com>
2024-12-05 15:07:47 -0800

    update, move user table into standalone component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,35 +1,14 @@
 import React from 'react'
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
+import UserTable from './UserTable';
 
 
-const UsersPage = async () => {
-  const res = await fetch(
-    'https://jsonplaceholder.typicode.com/users',
-  );
-  const users: User[] = await res.json();
+
+const UsersPage = () => {
 
   return (
     <>
       <h1>Users</h1>
-      <p>{new Date().toLocaleTimeString()}</p>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => <tr key={user.id}>
-            <th>{user.name}</th>
-            <th>{user.email}</th></tr>)}
-        </tbody>
-      </table>
+      <UserTable />
     </>
   )
 }
