337a79b9667b6ec58c362e1778ee7d44860d40df
Ryan Robertson <ryguyrobertson@gmail.com>
2024-12-05 17:08:36 -0800

    update new user route

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import UserTable from './UserTable';
+import Link from 'next/link';
 
 interface Props {
   searchParams: { sortOrder : string }
@@ -10,6 +11,7 @@ const UsersPage = async ( {searchParams :{ sortOrder }} : Props) => {
   return (
     <>
       <h1>Users</h1>
+      <Link href="/users/new" className='btn'>NEW USER</Link>
       <UserTable sortOrder={sortOrder} />
     </>
   )
