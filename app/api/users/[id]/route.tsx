561791a1f5b947b7f4f3f7530fe568801981f64b
Ryan Robertson <ryguyrobertson@gmail.com>
2024-12-09 10:33:43 -0800

    add PUT for updating user w/ id

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -6,5 +6,21 @@ export function GET(
     if (params.id > 10)
       return NextResponse.json({ error: 'User not Found'}, { status: 404});
     return NextResponse.json({id: 1, name: 'Ryan'});
+  }
 
-}
+
+// Update Function
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number}}) {
+    const body = await request.json();
+    // No name validation
+    if (!body.name)
+      return NextResponse.json({ error: 'Name is required'}, { status: 400})
+
+    // No matching id (Hard limit with no real DB)
+    if (params.id > 10)
+      return NextResponse.json({ error: 'User not found'}, { status: 400})
+
+    return NextResponse.json({ id: 1, name: body.name})
+  }
