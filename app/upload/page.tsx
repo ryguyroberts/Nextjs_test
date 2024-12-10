96e068deadfd7d695543ac5d0ccd02510d6bfe00
Ryan Robertson <ryguyrobertson@gmail.com>
2024-12-10 15:09:26 -0800

    install nextauth + add auth route for handler

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,11 +1,16 @@
 'use client';
-import React from 'react'
+import React, { useState } from 'react'
 import { CldUploadWidget } from 'next-cloudinary'
 
 
 const UploadPage = () => {
+
+  // States
+  const [publicId, setPublicId] = useState('');
+
   return (
-    <CldUploadWidget uploadPreset='uploadtest'>
+    <CldUploadWidget
+      uploadPreset='uploadtest'>
       {({ open }) => <button 
       className='btn btn-primary'
       onClick={() => open()}>Upload</button>}
